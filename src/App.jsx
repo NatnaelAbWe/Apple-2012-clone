a51d46c557f4a0f6d4ad906f80d55481634d6d5a
Natnael Abnew <natnaelanbewpro@gmail.com>
2025-08-26 19:40:25 +0300

    feat: add project image  assets

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,26 @@
 import "./App.css";
+import NavBar from "./Components/NavBar";
+import MacBookPro from "./Components/MacbookPro";
+import Iphone11 from "./Components/Iphone11";
+import Iphone11Pro from "./Components/Iphone11Pro";
+import WatchSeries from "./Components/WatchSeries";
+import AppleTv from "./Components/AppleTv";
+import IpadAndMacBook from "./Components/IpadAndMacBook";
+import Footer from "./Components/Footer";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <NavBar />
+      <MacBookPro />
+      <Iphone11 />
+      <Iphone11Pro />
+      <WatchSeries />
+      <AppleTv />
+      <IpadAndMacBook />
+      <Footer />
+    </>
+  );
 }
 
 export default App;
